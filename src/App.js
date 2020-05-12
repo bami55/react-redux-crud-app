80b5d42813a72d6ecf8acea9240b8c8708ca4750
bami55 <1992bami@gmail.com>
2020-05-12 12:40:03 +0900

    :sparkles: create class component and functional component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import React from 'react';
 function App() {
   return (
     <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange ={() => {console.log("I am clicked.")}} />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
     </React.Fragment>
   )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
